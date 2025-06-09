ef2a5df64558b1a72f266f454375ff39a9929adf
Omans16 <muhamadnurrohman007@gmail.com>
2025-06-09 21:00:44 +0700

    Memperbaiki dan menyinkronkan HistoryLens

--- Model_Load.py
+++ Model_Load.py
@@ -1,8 +1,11 @@
 from keras.models import model_from_json
 
-def load_model_from_files(json_path: str, weights_path: str):
+def load_model_from_files(json_path, weights_path):
     with open(json_path, "r") as json_file:
-        model_json = json_file.read()
-    model = model_from_json(model_json)
+        loaded_model_json = json_file.read()
+    
+    model = model_from_json(loaded_model_json)
     model.load_weights(weights_path)
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
+
